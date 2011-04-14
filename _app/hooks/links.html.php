7628e3243559cbbf13e9bcd5bcc697f44fa35eb0
Hisateru Tanaka <tanakahisateru@gmail.com>
2011-04-14 20:29:17 +0900

    step5: writing a hook script.

---
+++ _app/hooks/links.html.php
@@ -0,0 +1,7 @@
+<?php
+$this->links = array(
+    'PHP' => 'http://php.net/',
+    'Pinoco' => 'https://github.com/tanakahisateru/pinoco',
+    'PHPTAL' => 'http://phptal.org',
+    'Initializr' => 'http://initializr.com/',
+);
