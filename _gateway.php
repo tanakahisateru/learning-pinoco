748dff64f3c4e329b05c291e9a27fe0108ec4a52
Hisateru Tanaka <tanakahisateru@gmail.com>
2011-06-03 14:01:14 +0900

    pagoda setting 1st trying

--- _gateway.php
+++ _gateway.php
@@ -1,9 +1,21 @@
 <?php
 require_once '_app/lib/Pinoco.php';
-Pinoco::create("_app", array(
+$pinoco = Pinoco::create("_app", array(
 //    'use_mod_rewrite'  => TRUE,  // TRUE or FALSE default TRUE
-//    'use_path_info'    => TRUE,  // TRUE or FALSE default TRUE
+      'use_path_info'    => FALSE,  // TRUE or FALSE default TRUE
 //    'custom_path_info' => FALSE, // FALSE(auto) or string default FALSE
 //    'directory_index'  => "index.html index.php", // string like DirectoryIndex directive default "index.html index.php"
-))->run();
+));
+if (substr(PHP_OS, 0, 3) == 'WIN') {
+    if (file_exists('c:\\WINNT\\Temp\\')) {
+        $cachedir = 'c:\\WINNT\\Temp\\';
+    } else {
+        $cachedir = 'c:\\WINDOWS\\Temp\\';
+    }
+}
+else {
+    $cachedir = '/tmp';
+}
+$pinoco->renderers->html->cfg->phpCodeDestination = $cachedir;
+$pinoco->run();
 
